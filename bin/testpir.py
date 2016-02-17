6eed96c49023586fccc8cd9f2c0c7c11b6e7446c
beeaton <beeaton@gmail.com>
2016-02-17 01:01:42 +0000

    cleanup

--- bin/testpir.py
+++ bin/testpir.py
@@ -7,8 +7,13 @@ import RPi.GPIO as GPIO #read the GPIO pins
 GPIO.setmode(GPIO.BCM)
 
 pir_pin = 25
+ledPrevPin = 18
 
 GPIO.setup(pir_pin, GPIO.IN)                                    #Enable PIR to sense montion
+
+GPIO.setup(ledPrevPin, GPIO.OUT)                                #Previous LED Pin
+GPIO.output(ledPrevPin, GPIO.HIGH)
+
 
 LEDStartTime = time.time()
 MonitorStartTime = time.time()
@@ -33,7 +38,7 @@ try:
        if time.time()-LEDStartTime < LEDTimeout: #LED will be on for 10 seconds after montion is detected
             print "LED is on "+str(time.time()-LEDStartTime) 
        else:
-            print "LED is now off"
+            GPIO.output(ledPrevPin, GPIO.HIGH)
             
        if time.time()-MonitorStartTime < MonitorTimeout: #Monitor screensaver will stay off for 30 seconds while montion is detected
            print "Monitor is still on " +str(time.time()-MonitorStartTime) 
